3cddd815c8eef4ff4b9796902b294a8df77765d1
Jou0420 <jesuszavaleta55@gmail.com>
2025-11-03 21:56:27 -0300

    algo

---
+++ API/src/db.ts/enums.ts
@@ -0,0 +1,18 @@
+export enum EstadoPedido {
+  PENDIENTE = "pendiente",
+  PREPARACION = "preparacion",
+  ENVIADO = "enviado",
+  ENTREGADO = "entregado",
+  CANCELADO = "cancelado"
+}
+
+export enum EstadoPago {
+  PENDIENTE = "pendiente",
+  APROBADO = "aprobado",
+  RECHAZADO = "rechazado"
+}
+
+export enum TipoDocumento {
+  BOLETA = "boleta",
+  FACTURA = "factura"
+}
